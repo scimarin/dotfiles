5fc0c2d29748e8707fc19587579c939a6c452384
sergiu128 <sergiu128@protonmail.com>
2020-12-21 21:41:13 +0200

    add ethereum ticker in waybar

---
+++ .config/waybar/modules/btc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import requests
+
+btceur = 'https://www.bitstamp.net/api/v2/ticker/btceur/'
+
+data = requests.get(btceur).json()
+bid = float(data['bid'])
+ask = float(data['ask'])
+
+price = (bid + ask) / 2
+
+print(str(round(price, 1)))
